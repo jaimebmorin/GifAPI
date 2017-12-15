2a748d7c66ccf6addacb9c3c36ef885cc14ceed5
Jaime <jaimebmorin@gmail.com>
2017-12-14 23:05:51 -0600

    first commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+
+var animalArray = ["cat", "dog", "mouse", "horse", "dragon", "batman", "snake", "goku", "naruto", "mustang"];
+
+
+// create buttons
+for (var i = 0; i < animalArray.length; i++) {
+    var gifButton = $("<button>");
+    gifButton.text(animalArray[i]);
+    gifButton.attr("data-button", animalArray[i]);
+    $("#gif-button").append(gifButton);
+}
+
+//if an animal button is clicked
+$("#gif-button").on("click", function() {
+    var searchid = $(this).attr("data-button");
+    console.log(searchid);
+
+
+// I"ve grayed out this section of code until I can get the attribute data-button to come to this function.
+//   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+//   searchid + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+//  $.ajax({
+//    url: queryURL,
+//    method: "GET"
+//  }).done(function(response) {
+//    var results = response.data
+//    for (var i = 0; i < results.length; i++) {
+//    var animalDiv = $("<div>");
+//    var rating = results[i].rating;
+//    var p = $("<p>").text("Rating: " + rating);
+//    var animalImage = $("<img>");
+//    animalImage.attr("src", results[i].images.fixed_height.url);
+//    animalImage.prepend(p);
+//    animalDiv.prepend(animalImage);
+//    $("#gifs-appear-here").prepend(animalDiv);
+
+});
+
+
+
+
+
+
+//if the "submit" button is clicked
+$("#add-search").on("click", function() {
+    event.preventDefault();
+    var gifInfo = $("#gif-input").val().trim();
+    var gifButton = $("<button>");
+    gifButton.text(gifInfo);
+    gifButton.attr("data-button", gifInfo);
+    $("#gif-button").append(gifButton);
+
+});
+
+
+});
